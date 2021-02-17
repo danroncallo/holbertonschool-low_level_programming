1323ce3d1d680375761aa5bb57b45d8e339128d8
danroncallo <danielframirezr@gmail.com>
2021-02-17 03:35:56 +0000

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _strlen - Function that returns the length of a string.
+* @s: String
+* Return: Sizeof of a string
+*/
+
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
