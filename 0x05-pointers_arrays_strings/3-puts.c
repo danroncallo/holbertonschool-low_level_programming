b94ae1a47dec677d953f04c068632850e5d37c51
danroncallo <danielframirezr@gmail.com>
2021-02-17 03:37:08 +0000

    add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _puts - Function that prints a string, followed by a new line, to stdout.
+* @str: String
+*/
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
